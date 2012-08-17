8ac1ef2e0dcbd4d0d1e53d7c56ccad4794467ffe
xxhp <xiao_hb@qq.com>
2012-08-17 21:31:55 +0800

    Initial Commit

---
+++ NotificationCenterClearLikeButtonTests/NotificationCenterClearLikeButtonTests.h
@@ -0,0 +1,13 @@
+//
+//  NotificationCenterClearLikeButtonTests.h
+//  NotificationCenterClearLikeButtonTests
+//
+//  Created by  on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NotificationCenterClearLikeButtonTests : SenTestCase
+
+@end
---
+++ NotificationCenterClearLikeButtonTests/NotificationCenterClearLikeButtonTests.m
@@ -0,0 +1,32 @@
+//
+//  NotificationCenterClearLikeButtonTests.m
+//  NotificationCenterClearLikeButtonTests
+//
+//  Created by  on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NotificationCenterClearLikeButtonTests.h"
+
+@implementation NotificationCenterClearLikeButtonTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NotificationCenterClearLikeButtonTests");
+}
+
+@end
